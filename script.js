8c984ab0681fe92d760ed24a6cb2bd94d26014d1
Ibhassan01 <olorunnisolaibrahim46@gmail.com>
2023-10-28 10:30:05 +0100

    Added the counter function for unread notifications

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const notifications = document.querySelectorAll('.notification-bar');
 const markAll = document.querySelector('.mark-all');
+const notificationNum = document.getElementById('number')
+
+var numNotifications = parseInt(notificationNum.textContent);
+
 
 markAll.addEventListener('click', () => {
     notifications.forEach(notification => {
@@ -9,10 +13,21 @@ markAll.addEventListener('click', () => {
             dot.style.display = 'none';
         }
     })
+    numNotifications = 0;
+    updateNotificationNumber();
 })
+
+
+function updateNotificationNumber(){
+    notificationNum.textContent = numNotifications;
+}
 
 notifications.forEach(notification => {
     notification.addEventListener('click', () => {
+        if(!notification.classList.contains('read')){
+            numNotifications--;
+            updateNotificationNumber();
+        }
         notification.classList.add('read');
         const dot = notification.querySelector('.dot')
         if(dot){
